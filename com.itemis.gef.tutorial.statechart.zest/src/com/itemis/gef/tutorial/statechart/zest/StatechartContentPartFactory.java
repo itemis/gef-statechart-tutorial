15d6e17f7fe01291c203f168f8e238e6f1f0842c
mwienand <matthias.wienand@itemis.de>
2019-07-26 13:43:32 +0200

    Make Zest simple state resizable.

--- com.itemis.gef.tutorial.statechart.zest/src/com/itemis/gef/tutorial/statechart/zest/StatechartContentPartFactory.java
+++ com.itemis.gef.tutorial.statechart.zest/src/com/itemis/gef/tutorial/statechart/zest/StatechartContentPartFactory.java
@@ -3,6 +3,9 @@ package com.itemis.gef.tutorial.statechart.zest;
 import java.util.Map;
 
 import org.eclipse.gef.fx.nodes.Connection;
+import org.eclipse.gef.fx.nodes.GeometryNode;
+import org.eclipse.gef.geometry.planar.Dimension;
+import org.eclipse.gef.geometry.planar.RoundedRectangle;
 import org.eclipse.gef.mvc.fx.parts.IContentPart;
 import org.eclipse.gef.zest.fx.ZestProperties;
 import org.eclipse.gef.zest.fx.parts.EdgePart;
@@ -15,10 +18,12 @@ import com.itemis.gef.tutorial.statechart.model.FinalState;
 import com.itemis.gef.tutorial.statechart.model.InitialState;
 import com.itemis.gef.tutorial.statechart.visuals.FinalStateVisual;
 import com.itemis.gef.tutorial.statechart.visuals.InitialStateVisual;
+import com.itemis.gef.tutorial.statechart.visuals.SimpleStateVisual;
 import com.itemis.gef.tutorial.statechart.visuals.TransitionVisual;
 
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.text.Text;
 
 public class StatechartContentPartFactory extends ZestFxContentPartFactory {
 
@@ -40,9 +45,37 @@ public class StatechartContentPartFactory extends ZestFxContentPartFactory {
 					if (FinalState.class.getName().equals(label)) {
 						return new FinalStateVisual();
 					}
-					return super.doCreateVisual();
+					return new SimpleStateVisual();
+				}
+				
+				@Override
+				protected Text getLabelText() {
+					Group v = getVisual();
+					if (v instanceof SimpleStateVisual) {
+						return ((SimpleStateVisual) v).getLabel();
+					}
+					return null;
+				}
+				
+				@Override
+				public Dimension getVisualSize() {
+					Group visual = getVisual();
+					if (visual instanceof SimpleStateVisual) {
+						GeometryNode<RoundedRectangle> shape = ((SimpleStateVisual) visual).getShape();
+						return new Dimension(shape.getWidth(), shape.getHeight());
+					}
+					return super.getVisualSize();
 				}
 
+				@Override
+				public void setVisualSize(Dimension totalSize) {
+					Group visual = getVisual();
+					if (visual instanceof SimpleStateVisual) {
+						((SimpleStateVisual) visual).getShape().resize(totalSize.width, totalSize.height);
+					} else {
+						super.setVisualSize(totalSize);
+					}
+				}
 			};
 			injector.injectMembers(part);
 			return part;
@@ -58,7 +91,5 @@ public class StatechartContentPartFactory extends ZestFxContentPartFactory {
 		} else {
 			return super.createContentPart(content, contextMap);
 		}
-
 	}
-
 }
