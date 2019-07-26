17e11efc9c9bebb2bdf647da0a28dc1972032857
mwienand <matthias.wienand@itemis.de>
2019-07-26 13:42:58 +0200

    Adapt anchorage geometry to state visual.

--- com.itemis.gef.tutorial.statechart.zest/src/com/itemis/gef/tutorial/statechart/zest/CustomNodePartAnchorProvider.java
+++ com.itemis.gef.tutorial.statechart.zest/src/com/itemis/gef/tutorial/statechart/zest/CustomNodePartAnchorProvider.java
@@ -1,31 +1,35 @@
 package com.itemis.gef.tutorial.statechart.zest;
 
 import org.eclipse.gef.fx.anchors.DynamicAnchor;
+import org.eclipse.gef.fx.nodes.GeometryNode;
+import org.eclipse.gef.fx.utils.NodeUtils;
 import org.eclipse.gef.geometry.planar.IGeometry;
+import org.eclipse.gef.geometry.planar.RoundedRectangle;
+import org.eclipse.gef.mvc.fx.parts.IVisualPart;
 import org.eclipse.gef.zest.fx.providers.NodePartAnchorProvider;
 
 import com.itemis.gef.tutorial.statechart.visuals.FinalStateVisual;
 import com.itemis.gef.tutorial.statechart.visuals.InitialStateVisual;
+import com.itemis.gef.tutorial.statechart.visuals.SimpleStateVisual;
 
 import javafx.scene.Node;
 
 public class CustomNodePartAnchorProvider extends NodePartAnchorProvider {
 
-	@Override
-	protected IGeometry computeAnchorageReferenceGeometry(DynamicAnchor anchor) {
-		Node anchorage = anchor.getAnchorage();
-
-		if (anchorage instanceof InitialStateVisual) {
-			InitialStateVisual initialStateVisual = (InitialStateVisual) anchorage;
-			return initialStateVisual.getCircle().getGeometry();
+	protected IGeometry computeAnchorageReferenceGeometry(
+			DynamicAnchor anchor) {
+		IVisualPart<? extends Node> host = getAdaptable();
+		Node visual = host.getVisual();
+		if (visual instanceof InitialStateVisual) {
+			GeometryNode<? extends IGeometry> circle = ((InitialStateVisual) visual).getCircle();
+			return NodeUtils.localToParent(circle, NodeUtils.getShapeOutline(circle));
+		} else if (visual instanceof FinalStateVisual) {
+			GeometryNode<? extends IGeometry> circle = ((FinalStateVisual) visual).getOuterCircle();
+			return NodeUtils.localToParent(circle, NodeUtils.getShapeOutline(circle));
+		} else if (visual instanceof SimpleStateVisual) {
+			GeometryNode<RoundedRectangle> shape = ((SimpleStateVisual) visual).getShape();
+			return NodeUtils.localToParent(shape, NodeUtils.getShapeOutline(shape));
 		}
-
-		if (anchorage instanceof FinalStateVisual) {
-			FinalStateVisual finalStateVisual = (FinalStateVisual) anchorage;
-			return finalStateVisual.getOuterCircle().getGeometry();
-		}
-
 		return super.computeAnchorageReferenceGeometry(anchor);
 	}
-
 }
--- com.itemis.gef.tutorial.statechart/src/com/itemis/gef/tutorial/statechart/parts/StateAnchorProvider.java
+++ com.itemis.gef.tutorial.statechart/src/com/itemis/gef/tutorial/statechart/parts/StateAnchorProvider.java
@@ -2,42 +2,76 @@ package com.itemis.gef.tutorial.statechart.parts;
 
 import org.eclipse.gef.common.adapt.IAdaptable;
 import org.eclipse.gef.fx.anchors.DynamicAnchor;
+import org.eclipse.gef.fx.anchors.DynamicAnchor.AnchorageReferenceGeometry;
 import org.eclipse.gef.fx.anchors.IAnchor;
+import org.eclipse.gef.fx.nodes.GeometryNode;
+import org.eclipse.gef.fx.utils.NodeUtils;
+import org.eclipse.gef.geometry.planar.IGeometry;
+import org.eclipse.gef.geometry.planar.RoundedRectangle;
 import org.eclipse.gef.mvc.fx.parts.IVisualPart;
 
 import com.google.inject.Provider;
+import com.itemis.gef.tutorial.statechart.visuals.FinalStateVisual;
+import com.itemis.gef.tutorial.statechart.visuals.InitialStateVisual;
+import com.itemis.gef.tutorial.statechart.visuals.SimpleStateVisual;
 
-import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.binding.ObjectBinding;
 import javafx.scene.Node;
 
-public class StateAnchorProvider implements Provider<IAnchor>, IAdaptable.Bound<IVisualPart<? extends Node>> {
+public class StateAnchorProvider extends IAdaptable.Bound.Impl<IVisualPart<? extends Node>> implements Provider<IAnchor> {
 
-	private IVisualPart<? extends Node> host;
-	private IAnchor anchor;
-
-	@Override
-	public ReadOnlyObjectProperty<IVisualPart<? extends Node>> adaptableProperty() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IVisualPart<? extends Node> getAdaptable() {
-		return host;
-	}
-
-	@Override
-	public void setAdaptable(IVisualPart<? extends Node> adaptable) {
-		host = adaptable;
-	}
+	private DynamicAnchor anchor;
 
 	@Override
 	public IAnchor get() {
 		if (anchor == null) {
 			Node anchorage = getAdaptable().getVisual();
 			anchor = new DynamicAnchor(anchorage);
+			anchor.getComputationParameter(AnchorageReferenceGeometry.class)
+			.bind(new ObjectBinding<IGeometry>() {
+				{
+					// XXX: Binding value needs to be recomputed when the
+					// anchorage changes or when the layout bounds of the
+					// respective anchorage changes.
+					anchor.anchorageProperty()
+							.addListener((obs, ov, nv) -> {
+									if (ov != null) {
+										unbind(ov.boundsInLocalProperty());
+									}
+									if (nv != null) {
+										bind(nv.boundsInLocalProperty());
+									}
+									invalidate();
+								});
+					Node anchorage = anchor.getAnchorage();
+					if (anchorage != null) {
+						bind(anchorage.boundsInLocalProperty());
+					}
+				}
+
+				@Override
+				protected IGeometry computeValue() {
+					return computeAnchorageReferenceGeometry(anchor);
+				}
+			});
 		}
 		return anchor;
 	}
-
+	
+	protected IGeometry computeAnchorageReferenceGeometry(
+			DynamicAnchor anchor) {
+		IVisualPart<? extends Node> host = getAdaptable();
+		Node visual = host.getVisual();
+		if (visual instanceof InitialStateVisual) {
+			GeometryNode<? extends IGeometry> circle = ((InitialStateVisual) visual).getCircle();
+			return NodeUtils.localToParent(circle, NodeUtils.getShapeOutline(circle));
+		} else if (visual instanceof FinalStateVisual) {
+			GeometryNode<? extends IGeometry> circle = ((FinalStateVisual) visual).getOuterCircle();
+			return NodeUtils.localToParent(circle, NodeUtils.getShapeOutline(circle));
+		} else if (visual instanceof SimpleStateVisual) {
+			GeometryNode<RoundedRectangle> shape = ((SimpleStateVisual) visual).getShape();
+			return NodeUtils.localToParent(shape, NodeUtils.getShapeOutline(shape));
+		}
+		return NodeUtils.getShapeOutline(visual);
+	}
 }
